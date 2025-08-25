f16af482da4be27853114467722a76dd13d72f55
YiKuan <s345612s@gmail.com>
2025-08-25 21:49:42 +0800

    feat:完成footer

--- src/components/layout/footerComp.vue
+++ src/components/layout/footerComp.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="bg-backgroundSurface border-t-primaryActive border-t border-solid">頁尾</div>
+  <div class="bg-backgroundSurface border-t-primaryActive border-t-solid border">
+    <div class="text-primary flex flex-col items-center py-4">
+      <div>© 2025 FIRE 投資版-記帳App</div>
+      <div class="mt-2 flex gap-4">
+        <div class="border-r-solid border pr-4">
+          <a href="https://www.104.com.tw/" class="text-primary no-underline">by Hatchi</a>
+        </div>
+        <div class="">
+          <a href="https://github.com/HatchiX8/FIRE-Project" class="text-primary no-underline"
+            ><span>GitHub</span></a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 // ----------import----------
