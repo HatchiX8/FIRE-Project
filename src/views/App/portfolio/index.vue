5da7a87ab8fe81d07ecaf2781a9f6f8ba606251d
YiKuan <s345612s@gmail.com>
2025-08-27 21:38:16 +0800

    feat:建立投資組合圓餅圖與資金配置區塊

--- src/views/App/portfolio/index.vue
+++ src/views/App/portfolio/index.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>頁面page1</div>
+  <div>
+    <trendChart :chartData="data" />
+  </div>
 </template>
 <script setup lang="ts">
 // ----------import----------
 // 套件
 // 共用型別
 // 元件
+import { trendChart } from './comps/index';
 // 商業邏輯
 
 // ---------------------------
 
 // ----------區域----------
+const data = {
+  totalInvest: 170000,
+  cashInvest: 65000,
+  stockCost: 105000,
+  stockValue: 110000,
+  positionRatio: 0.617,
+  stockProfit: 5000,
+  profitRate: 4.76,
+};
 // -------------------------
 </script>
